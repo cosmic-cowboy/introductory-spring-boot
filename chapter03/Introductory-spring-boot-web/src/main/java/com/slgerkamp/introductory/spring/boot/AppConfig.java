40b7814495810eb45c91f7c4639c7f7486327f8e
hiroo takaku <slgerkamp@gmail.com>
2015-03-01 15:35:13 +0900

    chapter03：Web用プロジェクト作成

---
+++ chapter03/Introductory-spring-boot-web/src/main/java/com/slgerkamp/introductory/spring/boot/AppConfig.java
@@ -0,0 +1,35 @@
+package com.slgerkamp.introductory.spring.boot;
+
+import javax.sql.DataSource;
+
+import net.sf.log4jdbc.Log4jdbcProxyDataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+	
+    @Autowired
+    DataSourceProperties properties;
+    DataSource dataSource;
+
+    @Bean(destroyMethod = "close")
+    DataSource realDataSource() {
+        DataSourceBuilder factory = DataSourceBuilder
+                .create(this.properties.getClassLoader())
+                .url(this.properties.getUrl())
+                .username(this.properties.getUsername())
+                .password(this.properties.getPassword());
+        this.dataSource = factory.build();
+        return this.dataSource;
+    }
+
+    @Bean
+    DataSource dataSource() {
+        return new Log4jdbcProxyDataSource(this.dataSource);
+    }
+}
